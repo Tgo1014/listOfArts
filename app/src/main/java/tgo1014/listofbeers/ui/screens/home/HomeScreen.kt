37111f50a4e615cffe8959e1cffa095b13afac68
Tiago Araujo de Souza <tiago.araujo@monstar-lab.com>
2021-12-01 22:21:45 +0100

    feature: added hilt

---
+++ app/src/main/java/tgo1014/listofbeers/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package tgo1014.listofbeers.ui.screens.home
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun HomeScreen() {
+    val viewModel = hiltViewModel<HomeViewModel>()
+    Surface {
+
+    }
+}
