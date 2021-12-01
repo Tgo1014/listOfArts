a28dbeef681e09ade2f372758760a33d73693ce6
Tiago Araujo de Souza <tiago.araujo@monstar-lab.com>
2021-12-01 23:19:57 +0100

    fix: crash on start caused by moshi
    moshi don't support kotlin 1.6.0 yet

--- app/src/main/java/tgo1014/listofbeers/ui/MainActivity.kt
+++ app/src/main/java/tgo1014/listofbeers/ui/MainActivity.kt
@@ -3,9 +3,11 @@ package tgo1014.listofbeers
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import tgo1014.listofbeers.ui.screens.home.HomeScreen
 import tgo1014.listofbeers.ui.theme.ListOfBeersTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
