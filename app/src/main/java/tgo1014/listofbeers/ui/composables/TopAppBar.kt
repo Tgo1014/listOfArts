0825b8020f40347822fd25158508537c91a0c857
Tiago Araujo de Souza <tiago.araujo@monstar-lab.com>
2021-12-04 20:53:39 +0100

    refactor: switched from grid to column to support span for loading and filter

---
+++ app/src/main/java/tgo1014/listofbeers/ui/composables/TopAppBar.kt
@@ -0,0 +1,44 @@
+package tgo1014.listofbeers.ui.composables
+
+import androidx.compose.animation.rememberSplineBasedDecay
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsetLargeTopAppBar(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    actions: @Composable RowScope.() -> Unit = {},
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    scrollBehavior: TopAppBarScrollBehavior? = null
+) {
+
+    Surface(
+        color = backgroundColor,
+        modifier = modifier
+    ) {
+        LargeTopAppBar(
+            title = title,
+            actions = actions,
+            colors = TopAppBarDefaults.largeTopAppBarColors(containerColor = Color.Transparent),
+            modifier = Modifier.padding(contentPadding),
+            scrollBehavior = scrollBehavior
+        )
+    }
+}
--- app/src/main/java/tgo1014/listofbeers/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/tgo1014/listofbeers/ui/screens/home/HomeScreen.kt
@@ -4,25 +4,30 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,10 +36,10 @@ import com.google.accompanist.insets.LocalWindowInsets
 import com.google.accompanist.insets.navigationBarsHeight
 import com.google.accompanist.insets.rememberInsetsPaddingValues
 import com.google.accompanist.insets.ui.Scaffold
-import com.google.accompanist.insets.ui.TopAppBar
 import com.google.accompanist.pager.ExperimentalPagerApi
 import tgo1014.listofbeers.R
 import tgo1014.listofbeers.ui.composables.BeerComposable
+import tgo1014.listofbeers.ui.composables.InsetLargeTopAppBar
 import tgo1014.listofbeers.ui.theme.Amber700
 
 @OptIn(
@@ -43,39 +48,60 @@ import tgo1014.listofbeers.ui.theme.Amber700
     ExperimentalFoundationApi::class
 )
 @Composable
-fun HomeScreen() = Surface {
+fun HomeScreen() {
     val viewModel = hiltViewModel<HomeViewModel>()
     val beers by viewModel.beersFlow.collectAsState()
     val isLoading by viewModel.loadingFlow.collectAsState()
+    val lazyState = rememberLazyListState()
+    val scrollBehavior = remember { TopAppBarDefaults.enterAlwaysScrollBehavior() }
     Scaffold(
-        topBar = { Toolbar() },
+        topBar = { Toolbar(scrollBehavior) },
         bottomBar = { BottomSpacing() },
+        backgroundColor = MaterialTheme.colorScheme.background
     ) { contentPadding ->
         BoxWithConstraints {
             val cellSize = 170
-            val grizSize = (maxWidth.value / cellSize).toInt()
-            LazyVerticalGrid(
-                cells = GridCells.Fixed(grizSize),
+            val gridSize = (maxWidth.value / cellSize).toInt()
+            LazyColumn(
+                state = lazyState,
                 contentPadding = contentPadding,
                 verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)
+                    .nestedScroll(scrollBehavior.nestedScrollConnection)
             ) {
-                itemsIndexed(beers) { index, beer ->
-                    BeerComposable(beer)
-                    if (beers.lastIndex == index) {
-                        SideEffect { viewModel.onBottomReached() }
+                item { Filter() }
+                items(beers.chunked(gridSize)) { chunk ->
+                    Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                        val modifier = Modifier.weight(1f)
+                        chunk.forEach { beer ->
+                            BeerComposable(beer, modifier)
+                            if (beer == beers.lastOrNull()) {
+                                SideEffect { viewModel.onBottomReached() }
+                            }
+                        }
+                        // This avoids the last row to take all the space istead of just 1 "grid block"
+                        for (i in 1..(gridSize - chunk.size)) {
+                            Spacer(modifier)
+                        }
                     }
                 }
-                if (isLoading) {
-                    item {
-                        Progress(Modifier.size(cellSize.dp))
+                item {
+                    if (isLoading) {
+                        Progress(Modifier.fillMaxWidth())
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun Filter() = Row {
+    Text(text = "_Filter:_")
+    FilledTonalButton(onClick = { /*TODO*/ }) {
+        Text(text = "Init2")
     }
 }
 
@@ -92,15 +118,16 @@ private fun Progress(modifier: Modifier) {
 }
 
 @Composable
-private fun Toolbar() {
-    TopAppBar(
+private fun Toolbar(scrollBehavior: TopAppBarScrollBehavior) {
+    InsetLargeTopAppBar(
         title = { Text(stringResource(R.string.app_name)) },
         backgroundColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.95f),
+        modifier = Modifier.fillMaxWidth(),
+        scrollBehavior = scrollBehavior,
         contentPadding = rememberInsetsPaddingValues(
             LocalWindowInsets.current.statusBars,
             applyBottom = false,
         ),
-        modifier = Modifier.fillMaxWidth()
     )
 }
 
